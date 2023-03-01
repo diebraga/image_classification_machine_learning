c84b3c6ef5ee4293090ae1b73c84d2c10701f340
Diego Braga <diegobraga@hertz.com>
2023-03-01 14:15:55 +0000

    added to main

--- download.py
+++ download.py
@@ -5,36 +5,29 @@ from fastdownload import download_url
 import ssl
 from time import sleep
 import certifi
+
 ssl._create_default_https_context = ssl._create_unverified_context
 ssl_context = ssl.create_default_context(cafile=certifi.where())
 
-def search_images(term, max_images=30):
+def search_images(term, max_images=80):
     print(f"Searching for '{term}'")
     return L(ddg_images(term, max_results=max_images)).itemgot('image')
 
-searches = 'dog','park'
-path = Path('dog_or_park')
-
-# DOWLOAD ONE BIRD PIC
-# urls = search_images('dog photos', max_images=1)
-# urls[0]
-
-# dest = 'dog.jpg'
-# download_url(urls[0], dest, show_progress=False)
-
-# from fastai.vision.all import *
-# im = Image.open(dest)
-# im.to_thumb(256,256)
+searches = ['dog', 'cat', 'other']
+path = Path('classification')
 
 for o in searches:
     dest = (path/o)
     dest.mkdir(exist_ok=True, parents=True)
-    download_images(dest, urls=search_images(f'{o} photo'))
-    sleep(10)  # Pause between searches to avoid over-loading server
-    download_images(dest, urls=search_images(f'{o} sunny photo'))
-    sleep(10)
-    download_images(dest, urls=search_images(f'{o} cloudy photo'))
-    sleep(10)
+    if o == 'other':
+        download_images(dest, urls=search_images(f'{o} photos'))
+    else:
+        download_images(dest, urls=search_images(f'{o} photos'))
+        sleep(10)  # Pause between searches to avoid over-loading server
+        download_images(dest, urls=search_images(f'{o} park photos'))
+        sleep(10)
+        download_images(dest, urls=search_images(f'{o} walk photos'))
+        sleep(10)
     resize_images(path/o, max_size=400, dest=path/o)
 
 failed = verify_images(get_image_files(path))
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import axios from "axios";
+import { useDropzone } from "react-dropzone";
+import ProgressBar from "@ramonak/react-progress-bar";
+
 import "./App.css";
 
 type ImageFile = File & {
   preview: string;
 };
 
+const initialState = {
+  cat: "0",
+  dog: "0",
+  other: "0",
+};
+
 function App() {
   const [image, setImage] = useState<ImageFile | null>(null);
   const [imageUrl, setImageUrl] = useState<string>("");
   const [prediction, setPrediction] = useState(
-    {} as { prediction: string; probability: string }
+    initialState as typeof initialState
   );
 
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files[0]) {
-      const selectedImage = e.target.files[0];
-      setImage(selectedImage as ImageFile);
-      const imageUrl = URL.createObjectURL(selectedImage);
-      setImageUrl(imageUrl);
-    }
-  };
+  const handleDrop = useCallback((acceptedFiles: any) => {
+    const selectedImage = acceptedFiles[0];
+    setImage(selectedImage as ImageFile);
+    const imageUrl = URL.createObjectURL(selectedImage);
+    setImageUrl(imageUrl);
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop: handleDrop,
+  });
 
   const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData();
-    if (image) {
+    if (image && imageUrl.length) {
       formData.append("file", image);
       try {
         const response = await axios.post(
@@ -39,48 +50,54 @@ function App() {
           }
         );
         setPrediction(response.data);
+        console.log(response.data);
       } catch (error) {
         console.log(error);
       }
-    } else {
-      setPrediction({
-        prediction: "no picture was uploaded",
-        probability: "",
-      });
     }
   };
 
-  const isEmpty = prediction.prediction === "no picture was uploaded";
-
-  const isDog = prediction.prediction === "is dog";
-
-  const resultColor = !isEmpty && isDog ? "#2678fc" : "red";
   return (
     <form className="App" onSubmit={handleFormSubmit}>
-      <h1>Is it a dog?</h1>
+      <h1>Cat Vs Dog</h1>
       <div className="card">
-        <div>
-          <input type="file" accept="image/*" onChange={handleImageChange} />
+        <div
+          {...getRootProps()}
+          style={{
+            border: "1px solid white",
+            minHeight: "300px",
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <input {...getInputProps()} />
+          {!imageUrl && (
+            <p>Drag and drop an image file here, or click to select file</p>
+          )}
+          <div style={{ marginTop: "1rem" }}>
+            {imageUrl && (
+              <img src={imageUrl} alt="selected image" height={200} />
+            )}
+          </div>
         </div>
-        <div style={{ marginTop: "1rem" }}>
-          {imageUrl && <img src={imageUrl} alt="selected image" height={200} />}
+        <div style={{ display: "flex", justifyContent: "center", gap: "9px" }}>
+          <button type="submit">PREDICT</button>{" "}
+          <button type="button" onClick={() => {setImageUrl(""), setPrediction(initialState)}}>
+            REMOVE
+          </button>
         </div>
-
-        <button type="submit">PREDIC</button>
       </div>
-      <p
-        style={
-          isEmpty
-            ? { color: "red" }
-            : { color: resultColor, fontWeight: 700, fontSize: "35px" }
-        }
-      >
-        {prediction && prediction.prediction}
-      </p>
       <p className="read-the-docs">
         "By uploading a picture, this program will analyze the image and predict
-        whether it contains a dog or not."
+        whether it contains a dog or cat."
       </p>
+      {Object.entries(prediction).map(([key, value]) => (
+        <div key={key}>
+          <p>{key}</p>
+          <ProgressBar completed={parseInt(value)} />
+        </div>
+      ))}
     </form>
   );
 }
